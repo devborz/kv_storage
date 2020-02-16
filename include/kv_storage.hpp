b2bbc0a5fb133788a9e9fe15f3af54b14f45b536
Usman Turkaev <u.turkaev@yandex.ru>
2020-02-17 02:04:51 +0300

    Update kv_storage.hpp

--- include/kv_storage.hpp
+++ include/kv_storage.hpp
@@ -4,8 +4,28 @@
 #include <boost/thread.hpp>
 #include <cassert>
 #include <iostream>
+#include <string>
 #include "rocksdb/db.h"
 
-namespace kv_storage {
+using namespace boost::program_options;
 
+namespace kv_storage {
+void get_options(variables_map& vm, int argc, char* argv[]) {
+  options_description desc("Allowed options");
+  desc.add_options()("help", "выводим вспомогательное сообщение")(
+      "log-level", value<std::string>(),
+      "= \"info\"|\"warning\"|\"error\"\n"
+      "= default: \"error\"")("thread-count", value<std::string>(),
+                              "=\n= default: count of logical core")(
+      "output", value<std::string>(),
+      "= <path/to/output/storage.db>\n"
+      "= default: <path/to/input/dbcs-storage.db>");
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+}
+
+void work(int argc, char* argv[]) {
+  variables_map vm;
+  get_options(vm, argc, argv);
+}
 }  // namespace kv_storage
